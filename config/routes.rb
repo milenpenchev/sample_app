73f61c8389d64a7ac4e1033f2d61a74c9bebea48
Milen Penchev <milen.penchev@gmail.com>
2012-12-03 12:46:56 +0200

    Finish layout and routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,13 @@
 SampleApp::Application.routes.draw do
-  root to: 'static pages#home'
+  get "users/new"
+
+  root to: 'static_pages#home'
+
+  match '/signup', to: 'users#new'
   
-  match '/help', to: 'static pages#help' 
-  match '/about', to: 'static pages#about' 
-  match '/contact', to: 'static pages#contact'
+  match '/help', to: 'static_pages#help' 
+  match '/about', to: 'static_pages#about' 
+  match '/contact', to: 'static_pages#contact'
 
   # The priority is based upon order of creation:
   # first created -> highest priority.
